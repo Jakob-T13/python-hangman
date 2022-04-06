9c81ae9fede2ca4aee7500bd7419033e77155b28
Jakob Thunen <jthunen@dev-10.com>
2022-04-06 10:43:44 -0500

    added comment to GetRandomWord

--- hangman.py
+++ hangman.py
@@ -2,5 +2,5 @@ import random
 
 words_list = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "NewHampshire", "NewJersey", "NewMexico", "NewYork", "NorthCarolina", "NorthDakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "RhodeIsland", "SouthCarolina", "SouthDakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "WestVirginia", "Wisconsin", "Wyoming"]
 
-def GetRandomWord(lst):
+def GetRandomWord(lst):     #Choose a random word from a list of strings
     return lst[random.randint(0,len(lst)-1)]
